d0efd79b570c0c7b4aaadf0af6386428a363016f
freedomxuli <xl1508107377@163.com>
2018-09-13 22:54:49 +0800

    二维码和微信支付

---
+++ web/weixin/html/BuyPointsStart.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WxPayAPI;
+
+public partial class weixin_html_BuyPointsStart : System.Web.UI.Page
+{
+    public static string wxEditAddrParam { get; set; }
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            JsApiPay jsApiPay = new JsApiPay(this);
+            try
+            {
+                //调用【网页授权获取用户信息】接口获取用户的openid和access_token
+                jsApiPay.GetOpenidAndAccessToken();
+
+                //获取收货地址js函数入口参数
+                wxEditAddrParam = jsApiPay.GetEditAddressParameters();
+                ViewState["openid"] = jsApiPay.openid;
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<span style='color:#FF0000;font-size:20px'>" + "页面加载出错，请重试" + "</span>");
+            }
+        }
+    }
+}
