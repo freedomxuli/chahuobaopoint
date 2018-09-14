9aa507a2802cdc45457a4883ffe0f56c2e1c6238
Zhoye <512238273@qq.com>
2018-09-14 22:57:48 +0800

    开放购买积分

---
+++ web/App_Code/KFGM/KFGMMag.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SmartFramework4v2.Web.WebExecutor;
+using System.Data;
+using SmartFramework4v2.Data.SqlServer;
+using System.Text;
+using System.Data.SqlClient;
+using SmartFramework4v2.Web.Common.JSON;
+using SmartFramework4v2.Data;
+
+/// <summary>
+///KFGMMag 的摘要说明
+/// </summary>
+[CSClass("KFGMMag")]
+public class KFGMMag
+{
+    public KFGMMag()
+    {
+        //
+        //TODO: 在此处添加构造函数逻辑
+        //
+    }
+
+    [CSMethod("GetList")]
+    public object GetList(int pagnum, int pagesize,string yhm)
+    {
+
+        using (DBConnection dbc = new DBConnection())
+        {
+            try
+            {
+                int cp = pagnum;
+                int ac = 0;
+
+                string where = "";
+                if (!string.IsNullOrEmpty(yhm.Trim()))
+                {
+                    where += " and " + dbc.C_Like("b.UserXM", yhm.Trim(), LikeStyle.LeftAndRightLike);
+                }
+
+                string str = @"  select a.*,b.UserName,b.UserXM from tb_b_platpoints a left join tb_b_user b on a.UserID=b.UserID
+                        where a.status=0 ";
+                str += where;
+
+                //开始取分页数据
+                System.Data.DataTable dtPage = new System.Data.DataTable();
+                dtPage = dbc.GetPagedDataTable(str + " order by a.Points ", pagesize, ref cp, out ac);
+
+                return new { dt = dtPage, cp = cp, ac = ac };
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+
+    [CSMethod("SaveKFGM")]
+    public bool SaveUser(JSReader jsr)
+    {
+        using (DBConnection dbc = new DBConnection())
+        {
+            dbc.BeginTransaction();
+            try
+            {
+                var PlatPointId = jsr["PlatPointId"].ToString();
+                string str = "select * from tb_b_platpoints where PlatPointId=" + dbc.ToSqlValue(PlatPointId);
+                DataTable pdt = dbc.ExecuteDataTable(str);
+
+                if (pdt.Rows.Count>0)
+                {
+                    var userid = pdt.Rows[0]["UserID"].ToString();
+
+                    var saledt = dbc.GetEmptyDataTable("tb_b_plattosale");
+                    var saledr = saledt.NewRow();
+                    saledr["PlatToSaleId"] = Guid.NewGuid().ToString();
+                    saledr["UserID"] = userid;
+                    saledr["points"] = Convert.ToDecimal(jsr["points"].ToString());
+                    saledr["addtime"] = DateTime.Now;
+                    saledr["status"] = 0;
+                    saledr["discount"] = Convert.ToDecimal(jsr["discount"].ToString());
+                    saledr["discountmemo"] = jsr["discountmemo"].ToString();
+                    //saledr["memo"] = ;
+                    saledt.Rows.Add(saledr);
+                    dbc.InsertTable(saledt);
+
+                    var dt = dbc.GetEmptyDataTable("tb_b_platpoints");
+                    var dtt = new SmartFramework4v2.Data.DataTableTracker(dt);
+                    var dr = dt.NewRow();
+                    dr["PlatPointId"] = PlatPointId;
+                    var point = Convert.ToDecimal(pdt.Rows[0]["Points"].ToString()) - Convert.ToDecimal(jsr["points"].ToString());
+                    dr["Points"] = point;
+                    dt.Rows.Add(dr);
+                    dbc.UpdateTable(dt, dtt);
+
+                    str = "select * from tb_b_user where UserID='" + userid + "'";
+                    DataTable udt = dbc.ExecuteDataTable(str);
+
+                    var wlmc = "";
+                    if (udt.Rows.Count > 0)
+                    {
+                        wlmc = udt.Rows[0]["UserXM"].ToString();
+                    }
+
+                    var logdt = dbc.GetEmptyDataTable("tb_b_record");
+                    var logdr = logdt.NewRow();
+                    logdr["CaoZuoJiLuID"]=Guid.NewGuid().ToString();
+                    logdr["UserID"]=SystemUser.CurrentUser.UserID;
+                    logdr["CaoZuoLeiXing"]="开放购买积分";
+                    logdr["CaoZuoNeiRong"] = SystemUser.CurrentUser.UserName + DateTime.Now + "开放购买" + wlmc + jsr["points"]+"积分";
+                    logdr["CaoZuoTime"]=DateTime.Now;
+                    //logdr["CaoZuoRemark"]
+                    logdt.Rows.Add(logdr);
+                    dbc.InsertTable(logdt);
+                }
+                dbc.CommitTransaction();
+                return true;
+            }
+            catch (Exception ex)
+            {
+                dbc.RoolbackTransaction();
+                throw ex;
+            }
+        }
+    }
+
+}
---
+++ web/JS/KFGM/KFGM.js
@@ -0,0 +1,282 @@
+﻿//
+
+var pageSize = 15;
+var cx_yhm;
+//************************************数据源*****************************************
+var store = createSFW4Store({
+    data: [],
+    pageSize: pageSize,
+    total: 1,
+    currentPage: 1,
+    fields: [
+       { name: 'PlatPointId' },
+       { name: 'companyId' },
+       { name: 'UserID' },
+       { name: 'UserName' },
+       { name: 'UserXM' },
+       { name: 'Points' }
+    ],
+    onPageChange: function (sto, nPage, sorters) {
+        getList(nPage);
+    }
+});
+//************************************数据源*****************************************
+
+//************************************页面方法***************************************
+function getList(nPage) {
+    CS('CZCLZ.KFGMMag.GetList', function (retVal) {
+        store.setData({
+            data: retVal.dt,
+            pageSize: pageSize,
+            total: retVal.ac,
+            currentPage: retVal.cp
+        });
+    }, CS.onError, nPage, pageSize,  Ext.getCmp("cx_yhm").getValue());
+}
+
+
+function sq(id) {
+    if (privilege("申请积分_积分授权_编辑")){
+        Ext.MessageBox.confirm('提示', '是否要授权!', function (obj) {
+            if (obj == "yes") {
+                CS('CZCLZ.JFSQMag.JFSQ', function (retVal) {
+                    if (retVal) {
+                        getList(1);
+                    }
+                }, CS.onError, id,1);
+            }
+            else {
+                return;
+            }
+        });
+    }
+}
+
+function kfgm(id) {
+    var r = store.findRecord("PlatPointId", id).data;
+    var win = new addWin();
+    win.show(null, function () {
+        Ext.getCmp("PlatPointId").setValue(id);
+        Ext.getCmp("MaxPoint").setValue(r.Points);
+    });
+    
+   
+
+}
+//************************************页面方法***************************************
+
+//************************************弹出界面***************************************
+Ext.define('addWin', {
+    extend: 'Ext.window.Window',
+
+    height: 250,
+    width: 400,
+    layout: {
+        type: 'fit'
+    },
+    closeAction: 'destroy',
+    modal: true,
+    title: '开放购买积分',
+
+    initComponent: function () {
+        var me = this;
+        me.items = [
+            {
+                xtype: 'form',
+                id: 'addform',
+                bodyPadding: 10,
+                items: [
+                    {
+                        xtype: 'textfield',
+                        fieldLabel: 'ID',
+                        id: 'PlatPointId',
+                        name: 'PlatPointId',
+                        hidden: true,
+                    },
+                    {
+                        xtype: 'numberfield',
+                        id: 'MaxPoint',
+                        name: 'MaxPoint',
+                        hidden: true,
+                    },
+                    {
+                        xtype: 'numberfield',
+                        fieldLabel: '分数',
+                        id: 'points',
+                        name: 'points',
+                        allowBlank: false,
+                        allowDecimals: false,
+                        allowNegative: false,
+                        anchor: '100%'
+                    },
+                    
+                    {
+                        xtype: 'numberfield',
+                        fieldLabel: '优惠折扣',
+                        id: 'discount',
+                        name: 'discount',
+                        allowDecimals : true,    //是否允许小数
+                        decimalPrecision : 2,    // 精确的位数
+                        allowNegative : false, 
+                        minValue: 0,
+                        maxValue:1,
+                        allowBlank: false,
+                        anchor: '100%'
+                    },
+                    {
+                        xtype: "label",
+                        text: "*优惠折扣必须为介于0-1的两位小数！",
+                        style:"color:red;padding-left:100px;"
+                    },
+                    {
+                        xtype: 'textareafield',
+                        id: 'discountmemo',
+                        name: 'discountmemo',
+                        fieldLabel: '优惠折扣备注',
+                        allowBlank: false,
+                        anchor: '100%'
+                    },
+                ],
+                buttonAlign: 'center',
+                buttons: [
+                    {
+                        text: '确定',
+                        iconCls: 'dropyes',
+                        handler: function () {
+                            var point=Ext.getCmp("points").getValue();
+                            var maxpoint = Ext.getCmp("MaxPoint").getValue();
+                            if (point > maxpoint) {
+                                Ext.Msg.show({
+                                    title: '提示',
+                                    msg: '开放购买的积分不得超过其所有积分',
+                                    buttons: Ext.MessageBox.OK,
+                                    icon: Ext.MessageBox.INFO
+                                });
+                                return;
+                            }
+
+                            var form = Ext.getCmp('addform');
+                            if (form.form.isValid()) {
+                                var values = form.form.getValues(false);
+                                var me = this;
+                                CS('CZCLZ.KFGMMag.SaveKFGM', function (retVal) {
+                                    if (retVal) {
+                                        me.up('window').close();
+                                        getList(1);
+                                    }
+                                }, CS.onError, values);
+
+                            }
+                        }
+                    },
+                     {
+                         text: '取消',
+                         iconCls: 'back',
+                         handler: function () {
+                             this.up('window').close();
+                         }
+                     }
+                ]
+            }
+        ];
+        me.callParent(arguments);
+    }
+});
+//************************************弹出界面***************************************
+
+//************************************主界面*****************************************
+Ext.onReady(function () {
+    Ext.define('KFGMView', {
+        extend: 'Ext.container.Viewport',
+
+        layout: {
+            type: 'fit'
+        },
+
+        initComponent: function () {
+            var me = this;
+            me.items = [
+                {
+                    xtype: 'gridpanel',
+                    id: 'KFGMGrid',
+                    store: store,
+                    columnLines: 1,
+                    border: 1,
+                    columns: [Ext.create('Ext.grid.RowNumberer'),
+                        {
+                            xtype: 'gridcolumn',
+                            dataIndex: 'UserXM',
+                            sortable: false,
+                            menuDisabled: true,
+                            width: 400,
+                            text: '物流名称'
+                        },
+                        {
+                            xtype: 'gridcolumn',
+                            dataIndex: 'Points',
+                            sortable: false,
+                            menuDisabled: true,
+                            width: 200,
+                            text: '积分'
+                        },
+                        {
+                            xtype: 'gridcolumn',
+                            sortable: false,
+                            menuDisabled: true,
+                            dataIndex: 'PlatPointId',
+                            text: '操作',
+                            renderer: function (value, cellmeta, record, rowIndex, columnIndex, store) {
+                                str = "<a href='JavaScript:void(0)' onclick='kfgm(\"" + value + "\")'>开放购买</a>";
+                                return str;
+                            }
+                        }
+                    ],
+                    viewConfig: {
+
+                    },
+                    dockedItems: [
+                        {
+                            xtype: 'toolbar',
+                            dock: 'top',
+                            items: [
+                                {
+                                    xtype: 'textfield',
+                                    id: 'cx_yhm',
+                                    labelWidth: 60,
+                                    fieldLabel: '物流名称'
+                                },
+                                {
+                                    xtype: 'buttongroup',
+                                    title: '',
+                                    items: [
+                                        {
+                                            xtype: 'button',
+                                            iconCls: 'search',
+                                            text: '查询',
+                                            handler: function () {
+                                                getList(1);
+                                            }
+                                        }
+                                    ]
+                                }
+                                
+                            ]
+                        },
+                        {
+                            xtype: 'pagingtoolbar',
+                            displayInfo: true,
+                            store: store,
+                            dock: 'bottom'
+                        }
+                    ]
+                }
+            ];
+            me.callParent(arguments);
+        }
+    });
+
+    new KFGMView();
+
+    getList();
+})
+//************************************主界面*****************************************
